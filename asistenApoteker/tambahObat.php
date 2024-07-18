25a1accf192183e06bdd998565d4562c3f4b5d6d
davaattabrani2 <attabrani.dava@gmail.com>
2024-07-18 16:49:58 +0700

    fix bug tambahObat

--- asistenApoteker/tambahObat.php
+++ asistenApoteker/tambahObat.php
@@ -1,16 +1,13 @@
 <!DOCTYPE html>
 <?php
-
     require '../config.php';
 
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
-
     }
 
     // Query untuk tabel jenis
     $queryJenis = "SELECT id_jenis, nama_jenis FROM jenis ORDER BY nama_jenis";
-
     $resJenis = mysqli_query($conn, $queryJenis);
     if (!$resJenis) {
         die("Query failed: " . mysqli_error($conn));
@@ -33,20 +30,16 @@
 
 <head>
 
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     <meta name="description" content="">
     <meta name="author" content="">
 
     <title>Tambah Obat</title>
 
-
     <!-- Custom fonts for this template-->
     <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
@@ -72,14 +65,11 @@
                 <div class="sidebar-brand-text mx-3" style="font-size: smaller;">PT. Laju Dhikara Abadi</div>
             </a>
 
-
             <!-- Divider -->
             <hr class="sidebar-divider my-0">
 
-
             <!-- Nav Item - Beranda -->
             <li class="nav-item">
-
                 <a class="nav-link" href="index.php">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Beranda</span></a>
@@ -87,7 +77,6 @@
 
 
             <!-- Divider -->
-
             <hr class="sidebar-divider">
 
 
@@ -95,53 +84,38 @@
             <li class="nav-item active">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
                     aria-expanded="true" aria-controls="collapsePages">
-
                     <i class="fas fa-fw fa-folder"></i>
                     <span>Data Master</span>
                 </a>
                 <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
                         <a class="collapse-item" href="obat.php"><i class="fas fa-pills"></i> Obat</a>
-
                         <a class="collapse-item" href="supplier.php"><i class="fas fa-truck"></i> Supplier</a>
                         <a class="collapse-item" href="jenis.php"><i class="fas fa-share-alt"></i> Jenis</a>
                         <a class="collapse-item" href="satuan.php"><i class="fas fa-cog"></i> Satuan</a>
                         <a class="collapse-item" href="periode.php"><i class="fas fa-calendar"></i> Periode</a>
-
                     </div>
                 </div>
             </li>
             
-
             <!-- Nav Item - Tables -->
             <li class="nav-item">
-
                 <a class="nav-link" href="obatMasuk.php">
-
                     <i class="fas fa-fw fa-dolly-flatbed"></i>
-
                     <span>Obat Masuk</span></a>
             </li>
 
-
             
             <!-- Nav Item - Tables -->
             <li class="nav-item">
-
                 <a class="nav-link" href="penjualan.php">
-
                     <i class="fas fa-fw fa-money-bill-wave"></i>
-
                     <span>Penjualan</span></a>
             </li>
-
             
-
             <!-- Nav Item - Charts -->
             <li class="nav-item">
-
                 <a class="nav-link" href="pembelian.php">
-
                     <i class="fas fa-fw fa-shopping-cart"></i>
                     <span>Pembelian</span></a>
             </li>
@@ -149,21 +123,18 @@
             <!-- Divider -->
             <hr class="sidebar-divider d-none d-md-block">
 
-
             <!-- Sidebar Toggler (Sidebar) -->
             <div class="text-center d-none d-md-inline">
                 <button class="rounded-circle border-0" id="sidebarToggle"></button>
             </div>
 
         </ul>
-
         <!-- End of Sidebar -->
 
         <!-- Content Wrapper -->
         <div id="content-wrapper" class="d-flex flex-column">
 
             <!-- Main Content -->
-
             <div id="content">
 
                 <!-- Topbar -->
@@ -172,16 +143,13 @@
                     <!-- Sidebar Toggle (Topbar) -->
                     <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
                         <i class="fa fa-bars"></i>
-
                     </button>
 
                     <!-- Topbar Navbar -->
                     <ul class="navbar-nav ml-auto">
 
                         <!-- Nav Item - Search Dropdown (Visible Only XS) -->
-
                         <li class="nav-item dropdown no-arrow d-sm-none">
-
                             <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <i class="fas fa-search fa-fw"></i>
@@ -191,17 +159,13 @@
                                 aria-labelledby="searchDropdown">
                                 <form class="form-inline mr-auto w-100 navbar-search">
                                     <div class="input-group">
-
                                         <input type="text" class="form-control bg-light border-0 small"
                                             placeholder="Search for..." aria-label="Search"
-
                                             aria-describedby="basic-addon2">
                                         <div class="input-group-append">
                                             <button class="btn btn-primary" type="button">
-
                                                 <i class="fas fa-search fa-sm"></i>
                                             </button>
-
                                         </div>
                                     </div>
                                 </form>
@@ -211,16 +175,12 @@
                         <div class="topbar-divider d-none d-sm-block"></div>
 
                         <!-- Nav Item - User Information -->
-
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
-
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-
                                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">Asisten Apoteker</span>
                                 <img class="img-profile rounded-circle"
                                     src="../img/undraw_profile.svg">
-
                             </a>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
@@ -230,7 +190,6 @@
                                     Logout
                                 </a>
                             </div>
-
                         </li>
 
                     </ul>
@@ -241,76 +200,69 @@
                 <!-- Begin Page Content -->
                 <div class="container-fluid">
 
-
                     <div class="container-fluid">
                         <!-- DataTales Example -->
-
                         <div class="card shadow mb-4">
                             <div class="card-header py-3">
                                 <h6 class="m-0 font-weight-bold text-primary">Tambah Data Obat</h6>
-
                             </div>
                             <div class="card-body">
-                            <form method="POST" action="prosesTambahObat.php">
-
+                            <form method="post" action="prosesTambahObat.php">
                                 <div class="form-group">
-                                    <label for="id_obat">ID Obat:</label>
-                                    <input type="text" class="form-control" id="id_obat" name="id_obat" required>
-
+                                    <label for="nama_obat">Nama Obat</label>
+                                    <input type="text" class="form-control" id="nama_obat" name="nama_obat" placeholder="Masukkan Nama Obat">
                                 </div>
                                 <div class="form-group">
-                                    <label for="nama_obat">Nama Obat:</label>
-                                    <input type="text" class="form-control" id="nama_obat" name="nama_obat" required>
-                                </div>
-                                <div class="form-group">
-                                    <label for="jenis">Jenis:</label>
-
-                                    <select class="form-control" id="jenis" name="jenis" required>
-                                        <?php while ($rowJenis = mysqli_fetch_assoc($resJenis)) : ?>
-                                            <option value="<?= $rowJenis['id_jenis']; ?>"><?= $rowJenis['nama_jenis']; ?></option>
-
-                                        <?php endwhile; ?>
+                                    <label for="nama_jenis">Jenis</label>
+                                    <select class="form-control" id="nama_jenis" name="nama_jenis">
+                                        <option>Pilih Jenis</option>
+                                        <?php
+                                        while ($rowJenis = mysqli_fetch_assoc($resJenis)) {
+                                            echo "<option value='" . $rowJenis['id_jenis'] . "'>" . $rowJenis['nama_jenis'] . "</option>";
+                                        }
+                                        ?>
                                     </select>
                                 </div>
                                 <div class="form-group">
-                                    <label for="satuan">Satuan:</label>
-                                    <select class="form-control" id="satuan" name="satuan" required>
-                                        <?php while ($rowSatuan = mysqli_fetch_assoc($resSatuan)) : ?>
-                                            <option value="<?= $rowSatuan['id_satuan']; ?>"><?= $rowSatuan['nama_satuan']; ?></option>
-                                        <?php endwhile; ?>
+                                    <label for="satuan">Satuan</label>
+                                    <select class="form-control" id="nama_satuan" name="nama_satuan">
+                                        <option>Pilih Satuan</option>
+                                        <?php
+                                        while ($rowSatuan = mysqli_fetch_assoc($resSatuan)) {
 
+                                            echo "<option value='" . $rowSatuan['id_satuan'] . "'>" . $rowSatuan['nama_satuan'] . "</option>";
+                                        }
+
+                                        ?>
                                     </select>
+
                                 </div>
                                 <div class="form-group">
-
-                                    <label for="supplier">Supplier:</label>
-                                    <select class="form-control" id="supplier" name="supplier" required>
-                                        <?php while ($rowSupplier = mysqli_fetch_assoc($resSupplier)) : ?>
-
-                                            <option value="<?= $rowSupplier['id_supplier']; ?>"><?= $rowSupplier['nama_supplier']; ?></option>
-                                        <?php endwhile; ?>
-
+                                    <label for="supplier">Supplier</label>
+                                    <select class="form-control" id="nama_supplier" name="nama_supplier">
+                                        <option>Pilih Supplier</option>
+                                        <?php
+                                        while ($rowSupplier = mysqli_fetch_assoc($resSupplier)) {
+                                            echo "<option value='" . $rowSupplier['id_supplier'] . "'>" . $rowSupplier['nama_supplier'] . "</option>";
+                                        }
+                                        ?>
                                     </select>
                                 </div>
-                                <button type="submit" name="submit" class="btn btn-success">Tambah Data</button>
+                                <button type="submit" name="submit"class="btn btn-success">Tambah Data</button>
                                 <a href="obat.php" class="btn btn-secondary">Kembali</a>
                             </form>
                             </div>
-
                         </div>
     
                     </div>
                 </div>
-
                 <!-- /.container-fluid -->
 
             </div>
             <!-- End of Main Content -->
 
-
             <!-- Footer -->
             <footer class="sticky-footer bg-white">
-
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
                         <span>Copyright &copy; PT. Laju Dhikara Abadi 2024</span>
@@ -320,7 +272,6 @@
             <!-- End of Footer -->
 
         </div>
-
         <!-- End of Content Wrapper -->
 
     </div>
@@ -332,49 +283,50 @@
     </a>
 
     <!-- Logout Modal-->
-
     <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
         aria-hidden="true">
         <div class="modal-dialog" role="document">
-
             <div class="modal-content">
                 <div class="modal-header">
-
                     <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
                     <button class="close" type="button" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">×</span>
                     </button>
                 </div>
-
                 <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
                 <div class="modal-footer">
                     <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
                     <a class="btn btn-primary" href="../login.php">Logout</a>
                 </div>
-
             </div>
         </div>
     </div>
 
     <!-- Bootstrap core JavaScript-->
     <script src="../vendor/jquery/jquery.min.js"></script>
-
     <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
 
     <!-- Core plugin JavaScript-->
     <script src="../vendor/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for all pages-->
-
     <script src="../js/sb-admin-2.min.js"></script>
 
     <!-- Page level plugins -->
     <script src="../vendor/chart.js/Chart.min.js"></script>
 
     <!-- Page level custom scripts -->
-
     <script src="../js/demo/chart-area-demo.js"></script>
+    <script src="../js/demo/chart-pie-demo.js"></script>
+
+</body>
+
+</html>
+    <script src="../js/demo/chart-pie-demo.js"></script>
+
+</body>
+
+</html>
     <script src="../js/demo/chart-pie-demo.js"></script>
 
 </body>

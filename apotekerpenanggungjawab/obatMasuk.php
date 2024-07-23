318a46989b165a982c808ea2c135c0f684475271
davaattabrani2 <attabrani.dava@gmail.com>
2024-07-23 22:29:22 +0700

    fix bug

--- apotekerpenanggungjawab/obatMasuk.php
+++ apotekerpenanggungjawab/obatMasuk.php
@@ -226,6 +226,7 @@ if ($obat_masuk_result) {
                                                 <th>Obat</th>
                                                 <th>Jenis</th>
                                                 <th>Satuan</th>
+                                                <th>Supplier</th>
                                                 <th>Jumlah Penerimaan</th>
                                         
                                             </tr>
